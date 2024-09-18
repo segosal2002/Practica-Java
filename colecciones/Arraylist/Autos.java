fb84bd7f7f0431e0ce7e49995e5d86cfc3ed4bd3
segosal2002 <tomassalinas2002@gmail.com>
2024-09-18 12:41:14 -0300

    a

--- colecciones/Arraylist/Autos.java
+++ colecciones/Arraylist/Autos.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 public class Autos {
 
-List<String> nombres = new ArrayList<>();
+List<Auto> Vehiculos = new ArrayList<Auto>();
 
-    
+Auto auto1 = new Auto("Toyota", "Corolla",2007,20234.4);
+    Vehiculos.add(auto1);
 }
