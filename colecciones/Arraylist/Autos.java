351527467bf2905033422685f3879ee059339537
segosal2002 <tomassalinas2002@gmail.com>
2024-09-19 12:36:25 -0300

    aj

--- colecciones/Arraylist/Autos.java
+++ colecciones/Arraylist/Autos.java
@@ -1,9 +1,34 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 public class Autos {
+public static void main (String [] args) {
+    String Marca;
+    String Modelo;
+    int Anio;
+    double precioVenta;
 
-List<Auto> Vehiculos = new ArrayList<Auto>();
+    List<Auto> Vehiculos = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
 
-Auto auto1 = new Auto("Toyota", "Corolla",2007,20234.4);
-    Vehiculos.add(auto1);
+    do{
+        System.out.println("Ingrese la marca: ");
+        Marca =sc.nextLine();
+        if(Marca.isEmpty()){
+            break;
+        }
+        System.out.println("Ingrese el modelo: ");
+        Modelo = sc.nextLine();
+        System.out.println("Ingrese el Año: ");
+        Anio = sc.nextInt();
+        System.out.println("Ingrese el precio: ");
+        precioVenta = sc.nextDouble();
+
+        Auto auto = new Auto(Marca,Modelo,Anio,precioVenta);
+        Vehiculos.add(auto);
+    }while(true);
+    System.out.println(Vehiculos.size());
+
+
+}
 }
---
+++ colecciones/HashList/Pelicula.java
@@ -0,0 +1,6 @@
+public class Pelicula {
+    private int codigo;
+    private String titulo;
+    private float precio;
+    private String tipo;
+}
